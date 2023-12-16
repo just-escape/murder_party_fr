fed647d0cecde454be48742c0336491bbec250a5
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2023-12-16 11:37:59 +0100

    make admin interface work

--- murder_party_fr/dist_settings.py
+++ murder_party_fr/dist_settings.py
@@ -104,6 +104,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/PATH/TO/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
